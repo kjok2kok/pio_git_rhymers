089a99efd30dceb62000d6c346096673c7b3e9dd
Jan Koślin <240712@edu.p.lodz.pl>
2023-04-21 18:39:07 +0200

    6. Zastąpienie literałów deklaracjami stałych

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,7 +2,8 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
-    int totalRejected = 0;
+    public static final int REJECTED_COUNTER_INITIAL_VALUE = 0;
+    int totalRejected = REJECTED_COUNTER_INITIAL_VALUE;
 
     public int reportRejected() {
         return totalRejected;
